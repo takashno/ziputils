b0cea161636d1eff8b401b3eb7b7df44125962ba
takashno <nikku8130>
2020-04-21 21:12:11 +0900

    mod

---
+++ src/main/java/ZipUtils.java
@@ -0,0 +1,39 @@
+import net.lingala.zip4j.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
+import net.lingala.zip4j.model.ZipParameters;
+import net.lingala.zip4j.model.enums.EncryptionMethod;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ZipUtils {
+
+    public static void main(String[] args) {
+
+        var inputDir = "";
+        var outputDir = "";
+        var password = "";
+
+
+        ZipParameters zipParameters = new ZipParameters();
+        zipParameters.setEncryptFiles(true);
+        zipParameters.setEncryptionMethod(EncryptionMethod.ZIP_STANDARD);
+
+        try {
+            Files.list(Paths.get(inputDir)).forEach(x -> {
+                try {
+                    var fileName = x.getFileName().toString().substring(0,x.getFileName().toString().indexOf("."))+ ".zip";
+                    var zipFile = new ZipFile(Paths.get(outputDir, fileName).toString(), password.toCharArray());
+                    zipFile.setCharset(Charset.forName("windows-31j"));
+                    zipFile.addFile(x.toString(), zipParameters);
+                } catch (ZipException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
